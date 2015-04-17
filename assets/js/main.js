a57240eae2b6257e76765c3b979b300626309ac4
Anton Drobot <me@antondrobot.ru>
2015-04-17 13:10:10 +0500

    v0.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,9 @@
  */
 
 (function ($, document, window, undefined) {
-    $(document).ready(function () {
+    'use strict';
+
+    $(function () {
         $('input, textarea').placeholder();
-
-        $('.form__checkbox_styled, .form__radio_styled').after('<span class="form__control-styled"></span>');
-
-        $('.form__file_styled').wrap('<span class="form__file-styled-wrapper"></span>').after('<span class="form__file-styled">Выберите файл...</span>');
     });
-})($, document, window);
+})(JQuery, document, window);
