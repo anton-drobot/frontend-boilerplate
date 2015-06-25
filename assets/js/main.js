bcf7f4d63619bb9e1eef5e204086e9a9e1681e17
Anton Drobot <me@antondrobot.ru>
2015-06-26 01:54:24 +0500

    Update: improve main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,11 +20,31 @@
         $('html').addClass(bodyClasses.join(' '));
     };
 
+    var onClick = {};
+
+    var onSubmit = {};
+
     $(function () {
         preparePage();
 
         $('input, textarea').placeholder();
 
         $('.form__checkbox--pretty, .form__radio--pretty').after('<span class="form__control-indicator"></span>');
+
+        $(document).on('click', '.js-click', function (event) {
+            var action = $(this).data('action');
+
+            if (onClick[action] !== undefined) {
+                onClick[action].call(this, event);
+            }
+        });
+
+        $(document).on('submit', '.js-form', function (event) {
+            var action = $(this).data('action');
+
+            if (onSubmit[action] !== undefined) {
+                onSubmit[action].call(this, event);
+            }
+        });
     });
 })(jQuery, document, window);
